a5f1a0dc1790e5f9f43e27de109583a5f0e0680e
Alessandro Alviani <alvianialess@gmail.com>
2025-04-09 22:14:18 +0100

    extract pipeline works

--- extract/__init__.py
+++ extract/__init__.py
@@ -1,3 +1,4 @@
+from .add_image_description import insert_image_descriptions
 from .extract import convert_pdf_with_vlm
 
-__all__ = ["convert_pdf_with_vlm"]
+__all__ = ["convert_pdf_with_vlm", "insert_image_descriptions"]
--- extract/extract_pipeline.py
+++ extract/extract_pipeline.py
@@ -3,15 +3,22 @@ import sys
 
 import joblib
 
-# Add the directory containing this script to sys.path
+# Add the parent directory to the Python path
 current_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(current_dir)
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+# Import using relative imports for files in the same directory
+from add_image_description import insert_image_descriptions
 
 from extract import convert_pdf_with_vlm
 
 extraction = convert_pdf_with_vlm(
     "/Users/alessandro/Development/generalRAG/data/original/Fact-Sheets.pdf"
 )
+markdown_image = insert_image_descriptions(
+    extraction.document.export_to_markdown(), extraction.document.pictures
+)
 
 # Save
-joblib.dump(extraction, "output.joblib")
+joblib.dump(markdown_image, "output.joblib")
