a8345cff0a3d22ca601102b5b40bb861821a03cd
Alessandro Alviani <alvianialess@gmail.com>
2025-04-12 19:38:03 +0100

    works

--- knowledge_base/load_embeddings.py
+++ knowledge_base/load_embeddings.py
@@ -1,11 +1,16 @@
+import os
+import sys
+
 from sentence_transformers import SentenceTransformer
 
+# Add the parent directory to sys.path to allow importing from utils
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from utils.device import get_device
 from utils.load_env import get_env_vars
 
-from ..utils.device import get_device
-
 env_vars = get_env_vars()
-EMBED_MODEL_ID = env_vars["EMBED_MODEL_ID"]
+EMBED_MODEL_ID = env_vars.get("EMBED_MODEL_ID", "Alibaba-NLP/gte-Qwen2-7B-instruct")
 
 
 def load_embedding_model(model_name=EMBED_MODEL_ID):
